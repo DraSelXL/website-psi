0f81126a8193e4ca8e348ba5773b9af48f8368ae
Davin <christopherdavin99@gmail.com>
2021-07-17 19:18:45 +0700

    Auto stash before merge of "main" and "origin/main"

--- resources/views/inventory.blade.php
+++ resources/views/inventory.blade.php
@@ -2,8 +2,8 @@
     <span class="font-bold text-black text-4xl">
         Inventory
     </span>
-    <div class="h-14 w-96 bg-darkblue rounded-xl my-5 text-white font-bold p-2">
-        <div class="flex flex-row">
+    <div class="h-14 w-1/2 bg-darkblue rounded-xl my-5 text-white font-bold p-2">
+        <div class="flex flex-row content-start">
             @auth
                 <div class="flex-grow">{{ auth()->user()->gold }}</div>
                 <div class="flex-grow">{{ auth()->user()->points }}</div>
@@ -12,14 +12,30 @@
 
     </div>
 
-    <div class="flex flex-col w-7/12 h-50 bg-green-300 rounded-xl content-center">
-        <span class="flex-1 font-bold text-black text-4xl p-1 w-full">
-                <center>Materials</center>
-        </span>
-        <div class="flex-1 flex flex-wrap justify-evenly w-full overflow-y-scroll content-center">
-            @foreach($materials as $material)
-                <x-material-inventory :material="$material" />
-            @endforeach
+    <div class="flex flex-row content-between">
+
+        <!--Material-->
+        <div class="flex flex-col w-7/12 h-50 bg-green-300 rounded-xl content-center">
+            <span class="flex-1 font-bold text-black text-4xl p-1 w-full">
+                    <center>Materials</center>
+            </span>
+            <div class="flex-1 flex flex-wrap justify-evenly w-full overflow-y-scroll content-center">
+                @foreach($materials as $material)
+                    <x-material-inventory :material="$material" />
+                @endforeach
+            </div>
+        </div>
+
+
+        <!-- Achievement-->
+        <div class="flex flex-col w-5/12 h-50 bg-green-300 rounded-xl content-center ml-20">
+            <span class="font-bold text-black text-4xl p-1 w-full">
+                    <center>Achievement</center>
+            </span>
+            <div class="flex-1 flex flex-wrap justify-evenly w-full overflow-y-scroll content-center">
+
+            </div>
         </div>
     </div>
+
 </div>
